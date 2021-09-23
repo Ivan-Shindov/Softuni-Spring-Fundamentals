b59157de3b54a323151d24b2d51003002a73b74e
Ivan-Shindov <shindov162@gmail.com>
2021-09-24 01:38:05 +0300

    commit

---
+++ apps-softuni/Mobilelele/src/main/java/bg/softuni/Mobilelele/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package bg.softuni.Mobilelele.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public BaseEntity setId(long id) {
+        this.id = id;
+        return this;
+    }
+}
---
+++ apps-softuni/Mobilelele/src/main/java/bg/softuni/Mobilelele/entity/Brand.java
@@ -0,0 +1,11 @@
+package bg.softuni.Mobilelele.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "brands")
+public class Brand extends BaseEntity {
+
+    
+}
